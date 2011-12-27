22a5b74931059c235d4a7b3d96316004a2985227
Matthew Sporleder <msporleder@gmail.com>
2011-12-27 14:52:07 -0500

    fixed type conversion

--- free/free.c
+++ free/free.c
@@ -7,40 +7,42 @@
 #include <sys/types.h>
 #include <sys/swap.h>
 
-void getmem(void);
+void getmem(long);
 
 int
 main(int argc, char *argv[])
 {
-  int ch, kflag, mflag, gflag;
+  int ch;
+  long flag = 1;
   while ((ch = getopt(argc, argv, "kmg")) != -1)
     switch(ch) {
     case 'k':
-      kflag = 1;
+      flag = 1024;
       break;
     case 'm':
-      mflag = 1;
+      flag = 1024*1024;
       break;
     case 'g':
-      gflag = 1;
+      flag = 1024*1024*1024;
       break;
     }
+
   argc -= optind;
   argv += optind;
 
-  getmem();
+  getmem(flag);
   return 0;
 }
 
 /* get memory stats */
 void
-getmem(void)
+getmem(long flag)
 {
   struct uvmexp_sysctl uvmstat;
   size_t physm_s, uvmstat_s;
   uvmstat_s = sizeof(uvmstat);
   sysctlbyname("vm.uvmexp2", &uvmstat, &uvmstat_s, NULL, 0);
   printf("\ttotal\t\tused\t\tfree\n");
-  printf("Mem:\t%d\t%d\t%d\n", uvmstat.npages*uvmstat.pagesize, uvmstat.npages*uvmstat.pagesize-uvmstat.free*uvmstat.pagesize, uvmstat.free*uvmstat.pagesize);
-  printf("Swap:\t%d\t%d\t%d\n", uvmstat.swpages*uvmstat.pagesize, uvmstat.swpginuse*uvmstat.pagesize, uvmstat.swpages*uvmstat.pagesize-uvmstat.swpginuse*uvmstat.pagesize); 
+  printf("Mem:\t%8d\t%8d\t%8d\n", uvmstat.npages*uvmstat.pagesize/flag, (uvmstat.npages*uvmstat.pagesize-uvmstat.free*uvmstat.pagesize)/flag, uvmstat.free*uvmstat.pagesize/flag);
+  printf("Swap:\t%8d\t%8d\t%8d\n", uvmstat.swpages*uvmstat.pagesize/flag, uvmstat.swpginuse*uvmstat.pagesize/flag, (uvmstat.swpages*uvmstat.pagesize-uvmstat.swpginuse*uvmstat.pagesize)/flag); 
 }
