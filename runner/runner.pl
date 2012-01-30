68eac1cbcb5c722c8d7477ce6d5e551375c5a792
Matthew Sporleder <msporleder@gmail.com>
2012-01-30 13:29:07 -0500

    actually detach from term

--- runner/runner.pl
+++ runner/runner.pl
@@ -4,19 +4,31 @@ use strict;
 use POSIX;
 
 use Getopt::Std;
-getopts("p:t:a:");
-our($opt_p, $opt_t, $opt_a);
+getopts("p:t:a:f:o:");
+our($opt_p, $opt_t, $opt_a, $opt_f, $opt_o);
 
 my $prog = $opt_p || die usage();
 my $args = $opt_a || die usage();
 my $th   = $opt_t || die usage();
+my $pidf  = $opt_f || die usage();
+my $kido = $opt_o || die usage();
 
 sub usage
 {
-  print STDERR "$0 -p /path/to/prog -a \"args\" -t # forks\n";
+  print STDERR "$0 -p /path/to/prog -a \"args\" -t #forks -f pidfile -o kid_stdout\n";
 }
+close STDOUT;
+close STDIN;
+close STDERR;
 
 my %kids;
+
+if (fork())
+{
+  exit 0;
+}
+else
+{
 
 FORKIT:
 while (scalar keys(%kids) < $th)
@@ -27,8 +39,9 @@ while (scalar keys(%kids) < $th)
     #parent
     #when the child dies (system finishes and we hit exit 0)
     #it will reap and make another one
+    POSIX::setsid() or die "could not detach $!";
     $kids{$pid} = 0;
-    open(PIDF, ">/tmp/runner.pl.pid");
+    open(PIDF, ">$pidf");
     print PIDF "$$";
     close PIDF;
     while (1)
@@ -36,7 +49,7 @@ while (scalar keys(%kids) < $th)
       my $k = waitpid(-1,WNOHANG);
       if ($k > 0 and exists $kids{$k})
       {
-        print "parent: $k went away\n";
+        #print "parent: $k went away\n";
         delete $kids{$k};
       }
       if (scalar keys(%kids) < $th)
@@ -48,8 +61,16 @@ while (scalar keys(%kids) < $th)
   else
   {
     #child
+    close STDOUT;
+    close STDIN;
+    close STDERR;
+
+    open(STDOUT, '>>', $kido);
+    open(STDERR, ">&STDOUT");
     print "child: running $prog $args: $$\n";
     system("$prog $args");
     exit 0;
   }
+}
+
 }
